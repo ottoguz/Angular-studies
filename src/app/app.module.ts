d5cc7501c421060c4e5fd92c408d34b34bd89c13
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-09-12 15:01:14 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    NewComponentComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server', //NORMAL SELECTION(best)
+  //selector: '[app-server]', //ELEMENT SELECTION
+  //selector: '.app-server', //CLASS SELECTOR
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
