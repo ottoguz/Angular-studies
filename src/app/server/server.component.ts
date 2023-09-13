ddb74a3ffcf364936fad6cc359ee58c5f6314bd7
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-09-13 14:12:35 -0300

    Learning bundable property/events

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,18 @@ import { Component } from "@angular/core";
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverID: number = 10;
+  status: string = 'OFFLINE';
+  allowNewServer: boolean = false;
+  createServer: string = 'No servers created!'
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  onCreateServer() {
+    this.createServer = 'New Server Created!'
+  }
 }
